019b07acbc711758fa30e7d259ba787cfeadb6f4
veronikadogareva <veronika.dogareva@yandex.ru>
2023-09-18 16:40:08 +0700

    редачу фронт

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const { unauthorizedMessage } = require('../helpers/errorMessages');
 const { SECRET_KEY } = require('../helpers/config');
 
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   next(new UnauthorizedError('Ошибка авторизации'));
-  // }
-  // //* * извлекаем токен */
-  // const token = authorization.replace('Bearer ', '');
-  const token = req.cookies.jwt;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new UnauthorizedError('Ошибка авторизации'));
+  }
+  //* * извлекаем токен */
+  const token = authorization.replace('Bearer ', '');
+  // const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, SECRET_KEY);
